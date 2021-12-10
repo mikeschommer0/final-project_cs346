511f6c4283b17fc1b435dc4471b5ee79af61765e
mikeschommer0 <mikeschommer0@gmail.com>
2021-12-09 18:31:12 -0600

    pizzas now show up in order page

--- javascript/order.js
+++ javascript/order.js
@@ -1,14 +1,25 @@
 let add = document.querySelectorAll(".add").length;
 
 for (let i = 0; i < add; i++) {
+    document.querySelectorAll(".add")[i].addEventListener('click', getPizzas);
     document.querySelectorAll(".add")[i].addEventListener('click', addToOrder);
 }
 
-function addToOrder() {
+let selectedPizzas = [];
+function getPizzas(e) {
     const addPizza = document.querySelectorAll('input[type="checkbox"]:checked');
-    let selectedPizzas = [];
     addPizza.forEach((pizza) => {
         selectedPizzas.push(pizza.value);
     });
     alert(selectedPizzas);
+    e.preventDefault();
+}
+function addToOrder() {
+let orderList = document.getElementById('order');
+    for(let i = 0; i < selectedPizzas.length; i++) {
+        listItem = document.createElement('li');
+        listItem.innerHTML = selectedPizzas[i];
+        orderList.appendChild(listItem);
+    }
+    selectedPizzas = [];
 }
